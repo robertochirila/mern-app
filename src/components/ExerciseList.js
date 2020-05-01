5196c9b1cb6857ddfda9c50e1c2c70c4b14d3d57
Roberto Chirila <robertochirila@yahoo.com>
2020-05-01 15:21:13 +0100

    implemented frontend

---
+++ src/components/ExerciseList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class ExerciseList extends Component {
+    render() {
+        return (
+            <div className="container">
+                <p>Exercise list component</p>
+            </div>
+        )
+    }
+}
+
+export default ExerciseList
